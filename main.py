302efafd6f238ab957e96b07841a45cd8f46fba0
Rodri <r.serranvives@gmail.com>
2025-10-05 13:39:50 +0200

    feat: model check

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime 
 from weather_data_local_download import get_weather_data
-
+from pydantic import BaseModel
 
 app = FastAPI(title="Weather Analysis Microservice")
 
@@ -21,18 +21,29 @@ def health_check():
     """Endpoint para verificar que el servicio está funcionando"""
     return {"status": "healthy", "timestamp": datetime.now().isoformat()}
 
-@app.post("/weather_check")
-def weather_check(
-    lat: float,
-    lon: float,
-    radius: int,
-    year: int,
-    month: int,
-    start_day: int,
+
+
+class WeatherCheck(BaseModel):
+    lat: float
+    lon: float
+    radius: int
+    year: int
+    month: int
+    start_day: int
     end_day: int
+
+@app.post("/weather_check")
+async def weather_check(
+    weather_check: WeatherCheck
 ):
     """Endpoint para recibir parámetros de consulta meteorológica"""
-
+    lat = weather_check.lat
+    lon = weather_check.lon
+    radius = weather_check.radius
+    year = weather_check.year
+    month = weather_check.month
+    start_day = weather_check.start_day
+    end_day = weather_check.end_day
 
     return {
         "location": {
